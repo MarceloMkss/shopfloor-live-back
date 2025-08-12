c6b6037b987e2353403ecddcfc71de90322993f5
marcelo <>
2025-08-12 13:08:42 +0200

    Primer commit

---
+++ src/main/java/com/infotek/shopfloor_live/ShopfloorLiveApplication.java
@@ -0,0 +1,13 @@
+package com.infotek.shopfloor_live;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopfloorLiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopfloorLiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infotek/shopfloor_live/ShopfloorLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infotek.shopfloor_live;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopfloorLiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
